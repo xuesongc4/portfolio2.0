09d39844cf4d983200caa81e489546ef6777b2d2
xuesongc <xuesongc@gmail.com>
2017-04-29 20:13:59 -0700

    horizontal mobile

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,6 @@
 //-------------------- js for the navigation bar
 $(document).ready(function(){
     navSlide();
-    scrollThumb();
 });
 
 function navSlide(){
@@ -23,22 +22,3 @@ $(document).on('click', '.scroll', function(event){
         scrollTop: $( $.attr(this, 'href') ).offset().top
     }, 1000);
 });
-
-function scrollThumb(){
-    var thumb = $(".scroll-thumb");
-    var win = $(this);
-    var deltaScroll = null;
-
-    $(window).scroll(function(){
-        if(win.scrollTop() < 500){
-            deltaScroll = 357+(win.scrollTop()/2.2);
-            thumb.css("left",deltaScroll+"px");
-            thumb.css("display","block");
-        }
-        if(win.scrollTop()>500){
-            deltaScroll = 920+(win.scrollTop()/5.5);
-            thumb.css("left",deltaScroll+"px");
-            thumb.css("display","block");
-        }
-    });
-}
